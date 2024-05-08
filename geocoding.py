672239a7e03e4ee5e3199e6d5e7ccf2de49730c9
Ryan Lee <sangwon.r.lee@gmail.com>
2024-05-08 16:34:46 -0400

    located latitude and longitude in response

--- geocoding.py
+++ geocoding.py
@@ -1,13 +1,11 @@
 import googlemaps
 
-api_key = ""
 
 def geocode_from_address(address, key = api_key):
     gmaps = googlemaps.Client(key = key)
     geocode_result = gmaps.geocode(address)
-    print(geocode_result)
-    return geocode_result
+    return geocode_result[0]["geometry"]["location"]
 
 if __name__ == '__main__':
     address = "Jeremy Ranch Park & Ride, Park City, UT"
-    geocode_from_address(address)
+    print(geocode_from_address(address))
