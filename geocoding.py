56a198f1e72daded22341a555e464a0d66d4f370
Ryan Lee <sangwon.r.lee@gmail.com>
2024-05-08 16:42:22 -0400

    Completed geocoding function, moving on to reading and writing from a file

--- geocoding.py
+++ geocoding.py
@@ -1,10 +1,28 @@
 import googlemaps
+from openpyxl import Workbook
+import csv
 
+api_key = ""
 
 def geocode_from_address(address, key = api_key):
+    """
+    Use the googlemaps API to geocode an address (turn a physical address into latitude and longitude)
+
+    Input:
+    address (str): A human readable address
+    key (str): A Google Maps Platform API key 
+
+    Return:
+    (dict): a dictionary containing the latitude and longitude as decimal values 
+    """
     gmaps = googlemaps.Client(key = key)
     geocode_result = gmaps.geocode(address)
-    return geocode_result[0]["geometry"]["location"]
+
+    # If the address cannot be geocoded, just return None 
+    try:
+        return geocode_result[0]["geometry"]["location"]
+    except:
+        return None
 
 if __name__ == '__main__':
     address = "Jeremy Ranch Park & Ride, Park City, UT"
