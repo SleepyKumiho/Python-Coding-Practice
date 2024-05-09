66b2e44b3609136f5698b7fd7ce52d9f27edc719
Ryan Lee <sangwon.r.lee@gmail.com>
2024-05-08 22:30:20 -0400

    set up the Excel reading function, cleaning up

--- geocoding.py
+++ geocoding.py
@@ -13,16 +13,18 @@ def geocode_from_address(address, key = api_key):
     key (str): A Google Maps Platform API key 
 
     Return:
-    (dict): a dictionary containing the latitude and longitude as decimal values 
+    latitude, longitude: Tuple of floats represeting the decimal latitude and longitude, or None if it could not be found
     """
     gmaps = googlemaps.Client(key = key)
     geocode_result = gmaps.geocode(address)
 
     # If the address cannot be geocoded, just return None 
     try:
-        return geocode_result[0]["geometry"]["location"]
+        latitude = geocode_result[0]["geometry"]["location"]["lat"]
+        longitude = geocode_result[0]["geometry"]["location"]["lng"]
+        return latitude, longitude
     except:
-        return None
+        return None, None
 
 def geocode_file_excel(input_file_name, output_file_name, key = api_key):
     input_wb = openpyxl.load_workbook(filename = input_file_name)
@@ -30,11 +32,23 @@ def geocode_file_excel(input_file_name, output_file_name, key = api_key):
     output_wb = openpyxl.Workbook()
     output_ws = output_wb.active
 
-    row_num = 1
-    for row in input_ws.values:
-        print(row[1])
-        output_ws.cell(row = row_num, column = len(row)+1, value = row[0])
-        row_num += 1
+    for row_num, row in enumerate(input_ws.values):
+
+        address = row[-1]
+        # The first row is colulmn names
+        if row_num == 0:
+            output_ws.cell(row = row_num+1, column = 1, value = address)
+            output_ws.cell(row = row_num+1, column = 2, value = "Latitude")
+            output_ws.cell(row = row_num+1, column = 3, value = "Longitude")
+            print(address, "lat", "long")
+            continue
+
+        lat, long = geocode_from_address(address, key)
+        print(address, lat, long)
+        output_ws.cell(row = row_num+1, column = 1, value = address)
+        output_ws.cell(row = row_num + 1, column = 2, value = lat)
+        output_ws.cell(row = row_num + 1, column = 3, value = long)
+
     output_wb.save(output_file_name)
 
     return 1
@@ -46,4 +60,4 @@ def geocode_file_csv(input_file_name, output_file_name, key = api_key):
 if __name__ == '__main__':
     #address = "Jeremy Ranch Park & Ride, Park City, UT"
     #print(geocode_from_address(address))
-    geocode_file_excel("../../../Desktop/test.xlsx", "../../../Desktop/output.xlsx", 5)
+    geocode_file_excel("../../../Desktop/test.xlsx", "../../../Desktop/output.xlsx")
